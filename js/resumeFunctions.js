4df4b1d1c6a7803fe78d4df3dfeb5f573dd32da6
Cody Nicholson <codynicholson96@gmail.com>
2017-07-14 21:15:45 -0500

    Fix pics

--- js/resumeFunctions.js
+++ js/resumeFunctions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         var clickedPicSrc = $(this).prop('src');
         var clickedPicAlt = $(this).prop('alt');
         var captionText = document.getElementById("caption");
-        clickedPicSrc = clickedPicSrc.slice(-1000,-13)+"L"+clickedPicSrc.slice(-4);
+        clickedPicSrc = clickedPicSrc.slice(-1000,-13)+"L"+clickedPicSrc.slice(-13);
         captionText.innerHTML = clickedPicAlt;
         $('#myModal img').prop('src', clickedPicSrc);
         $('#myModal img').prop('alt', clickedPicAlt);
